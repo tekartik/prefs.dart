22d0a598876177ce55400de8132149bbf403ab10
alex <alex@tekartik.com>
2020-11-27 17:46:54 +0100

    fix: travis publish to none

--- prefs_flutter/tool/travis.dart
+++ prefs_flutter/tool/travis.dart
@@ -1,22 +1,5 @@
-import 'dart:io';
-
-import 'package:process_run/shell.dart';
-import 'package:tekartik_common_utils/bool_utils.dart';
-
-bool get runningOnTravis => parseBool(Platform.environment['TRAVIS']) == true;
+import 'package:dev_test/package.dart';
 
 Future main() async {
-  var shell = Shell();
-
-  await shell.run('''
-
-flutter analyze
-# flutter test
-
-''');
-
-  if (!runningOnTravis) {
-    // Skip test on travis
-    await shell.run('flutter test');
-  }
+  await packageRunCi('.');
 }
